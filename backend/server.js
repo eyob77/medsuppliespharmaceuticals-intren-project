aed45e5621742a432afaa4023a6291b29228b310
Eyob <eyobbirhanu111000@gmail.com>
2025-09-20 15:45:36 +0300

    test4

--- backend/server.js
+++ backend/server.js
@@ -51,12 +51,12 @@ app.use("/api/message",messageRoutes);
 app.use("/api/setting",settingRoutes);
 app.use("/api/analytics",analyticsRoute);
 
-if(process.env.NODE_ENV === "production"){
-  app.use(express.static(path.join(__dirname,"/admin2/dist")));
-  app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,"admin2","dist","index.html"))
-  })
-}
+// if(process.env.NODE_ENV === "production"){
+//   app.use(express.static(path.join(__dirname,"/admin2/dist")));
+//   app.get("*",(req,res)=>{
+//     res.sendFile(path.resolve(__dirname,"admin2","dist","index.html"))
+//   })
+// }
 
 
 app.listen(PORT, () => {
